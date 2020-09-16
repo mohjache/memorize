66a06afaedee14d3c217255527e2471409a1361a
Anaru Herbert <anaru.herbert@ellumehealth.com>
2020-09-17 00:14:18 +1000

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Anaru Herbert on 17/9/20.
+//  Copyright © 2020 Anaru Herbert. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
